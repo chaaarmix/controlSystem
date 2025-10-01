9b3e41f2a5be9683847bdf0adbca109d40b36efa
Alexandra <lisencoalexandra@gmail.com>
2025-10-01 23:04:28 +0300

    модель role_code

---
+++ backend/internal/models/role_code.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type RoleCode struct {
+	ID        uint      `gorm:"primaryKey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+
+	Role Role   `gorm:"type:varchar(20);uniqueIndex;not null"`
+	Code string `gorm:"type:varchar(10);uniqueIndex;not null"` // шестизначный код как строка
+}
