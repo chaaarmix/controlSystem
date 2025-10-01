1cb197d48e180e5192d0b31a7cc0f3551b4126c5
Alexandra <lisencoalexandra@gmail.com>
2025-10-01 23:02:19 +0300

    модель user

---
+++ backend/internal/models/user.go
@@ -0,0 +1,23 @@
+package models
+
+import "time"
+
+type Role string
+
+const (
+	RoleAdmin    Role = "admin"
+	RoleManager  Role = "manager"
+	RoleEngineer Role = "engineer"
+	RoleCustomer Role = "customer" // руководитель/заказчик
+)
+
+type User struct {
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+
+	FullName string `gorm:"not null" json:"full_name"`
+	Email    string `gorm:"uniqueIndex;not null" json:"email"`
+	Password string `gorm:"not null" json:"-"` // hashed
+	Role     Role   `gorm:"type:varchar(20);not null" json:"role"`
+}
