c35a53a205c2b3d24f6932898e5e60d69644a2b9
Alexandra <lisencoalexandra@gmail.com>
2025-10-02 03:04:12 +0300

    роутинг update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Homepage from "./pages/Homepage/Homepage"; // обычная домашняя страница
+import Homepage from "./pages/Homepage/Homepage";
+import ProjectsPage from "./pages/Projects/ProjectsPage"; // обычная домашняя страница
 
 function App() {
     const isAuthenticated = Boolean(localStorage.getItem("token")); // пример авторизации
@@ -19,6 +20,14 @@ function App() {
                     element={
                         <ProtectedRoute isAuthenticated={isAuthenticated}>
                             <Homepage />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/projects"
+                    element={
+                        <ProtectedRoute isAuthenticated={isAuthenticated}>
+                            <ProjectsPage />
                         </ProtectedRoute>
                     }
                 />
